1cfecc99e2998d5d11f7831a82277e532ac6228d
Anatame <anata.me97@gmail.com>
2021-06-27 05:09:58 +0530

    added updated popup files and added css with no minify to html to build because parcel errors when using minify

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,1 +1,10 @@
-document.querySelector('h1').style.backgroundColor = 'Red';
+// Initialize button with user's preferred color
+let authorizeBtn = document.getElementById("authorize")
+
+authorizeBtn.addEventListener("click", (e) => {
+   chrome.runtime.sendMessage({ msg: 'login' }, (response) => {
+      if (response === 'success') window.location.replace('./popup-sign-out.html')
+   })
+})
+
+
