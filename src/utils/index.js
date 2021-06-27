3763571b900eaa4a0f9e12536ee9fecdc3a0f50a
Anatame <anata.me97@gmail.com>
2021-06-27 05:30:35 +0530

    postData fun added:

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 import getParamByName from "./getParamByName";
 import getDiscordUri from "./getDiscordUri";
+import postData from "./postData";
 
 export default {
    getParamByName,
-   getDiscordUri
+   getDiscordUri,
+   postData,
 }
