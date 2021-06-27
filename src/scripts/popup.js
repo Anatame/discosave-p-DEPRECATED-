bd3b96246dbdab7e4ca8986c96e739e826ad6aba
Anatame <anata.me97@gmail.com>
2021-06-27 17:38:19 +0530

    added sign out page

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,10 +1,12 @@
-// Initialize button with user's preferred color
 let authorizeBtn = document.getElementById("authorize")
 
+
+
 authorizeBtn.addEventListener("click", (e) => {
-   chrome.runtime.sendMessage({ msg: 'login' }, (response) => {
-      if (response === 'success') window.location.replace('./popup-sign-out.html')
-   })
+   window.location.replace('./signOut.html')
+   // chrome.runtime.sendMessage({ msg: 'login' }, (response) => {
+   //    if (response === 'success') window.location.replace('../html/sign-out.html')
+   // })
 })
 
 
